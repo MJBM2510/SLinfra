1aef0639af0111102dddaf13a328ab3b8e6daff9
MJ <mohamnarut@gmail.com>
2026-01-24 00:03:03 +0330

    updating project structure

--- __init__.py
+++
@@ -1,5 +0,0 @@
-__version__ = "1.0.0"
-AUTHOR = "MJBM2510"
-
-from .state_tracker import StateTracker, Status
-from .file_logger import FileLogger
---
+++ infra/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "1.0.0"
+AUTHOR = "MJBM2510"
+
+from infra.state_tracker import StateTracker, Status
+from infra.file_logger import FileLogger
