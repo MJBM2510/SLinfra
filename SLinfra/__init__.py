341fe40d56a8d4e7cf012efca27375702d636ae7
MJ <mohamnarut@gmail.com>
2026-01-24 21:47:44 +0330

    folder name updated

---
+++ SLinfra/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.1.2"
+AUTHOR = "MJBM2510"
+
+from SLinfra.state_tracker import StateTracker, Status
+from SLinfra.file_logger import FileLogger
--- infra/__init__.py
+++
@@ -1,5 +0,0 @@
-__version__ = "0.1.2"
-AUTHOR = "MJBM2510"
-
-from infra.state_tracker import StateTracker, Status
-from infra.file_logger import FileLogger
